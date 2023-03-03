4638ee75df1521abb610ac7e3033dc719fd399b4
Eduardo Escaleira <duescaleira@gmail.com>
2023-03-03 14:17:17 -0300

    criada as ligacoes de iboulet e ibaction

--- 17. DesafioTabBar/Tela02VC.swift
+++ 17. DesafioTabBar/Tela02VC.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class Tela02VC: UIViewController {
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,15 +20,5 @@ class Tela02VC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
